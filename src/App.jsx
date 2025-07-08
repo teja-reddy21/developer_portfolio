a83f2e9a7cbe17ef5e06c9e0be8ca46578c38d09
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-07-08 20:37:32 +0530

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import Navbar from './Componants/Navbar/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='bg-[#171d32] h-auto w-full overflow-hidden'>
+         <div className="min-h-screen bg-black text-white flex items-center justify-center"></div>
+          <h1 className="text-4xl font-bold">Tailwind is finally working! 🎉</h1>
+        <Navbar/>
+      </div>
+    </>
+  )
+}
+
+export default App
