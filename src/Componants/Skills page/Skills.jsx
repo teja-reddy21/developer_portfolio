bd5131581d78bac1d5da1c9fc81a8cdb5b130540
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-07-10 21:58:34 +0530

    skills section

---
+++ src/Componants/Skills page/Skills.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './skills.css'
+const Skills = () => {
+  return (
+      <section id="skills" className="skills">
+      <h2>Skills</h2>
+      <ul className="skill-list">
+        <li>React JS</li>
+        <li>JavaScript</li>
+        <li>HTML & CSS</li>
+        <li>Git & GitHub</li>
+        <li>Responsive Design</li>
+      </ul>
+    </section>
+  )
+}
+
+export default Skills
