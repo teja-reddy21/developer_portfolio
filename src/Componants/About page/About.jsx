63c1eaf398484c074e445a093e704ccbffa8f96b
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-07-10 21:55:51 +0530

    about me section

---
+++ src/Componants/About page/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './about.css'
+const About = () => {
+  return (
+    <section id="about" className="about">
+      <h2>About Me</h2>
+      <p>
+        I'm a passionate frontend developer with strong knowledge in React JS. I love building responsive web applications and continuously improving my skills by learning new technologies.
+      </p>
+    </section>
+  )
+}
+
+export default About
