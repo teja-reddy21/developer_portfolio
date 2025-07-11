1f9fdf6cb83a20d5e797dafe0465853e15694e48
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-07-11 14:40:14 +0530

    added the icons

--- src/Componants/Skills page/Skills.jsx
+++ src/Componants/Skills page/Skills.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
 import './skills.css'
+
+// import skill logos
+import reactLogo from '../../assets/icons/react.png'
+import jsLogo from '../../assets/icons/js.png'
+import html5Logo from '../../assets/icons/html-5.png'
+import CssLogo from '../../assets/icons/Web Css.png'
+import gitLogo from '../../assets/icons/git.png'
+import gitHubLogo  from '../../assets/icons/github.png'
+import responsiveLogo from '../../assets/icons/responsive.png'
+ 
+const skills = [
+  { name: "React JS", logo: reactLogo },
+  { name: "JavaScript", logo: jsLogo },
+  { name: "HTML5", logo: html5Logo },
+  {name: "CSS", logo: CssLogo},
+  { name: "Git", logo: gitLogo },
+  {name: "GitHub", logo: gitHubLogo},
+  { name: "Responsive Design", logo: responsiveLogo }
+];
+
 const Skills = () => {
   return (
       <section id="skills" className="skills">
       <h2>Skills</h2>
       <ul className="skill-list">
-        <li>React JS</li>
-        <li>JavaScript</li>
-        <li>HTML & CSS</li>
-        <li>Git & GitHub</li>
-        <li>Responsive Design</li>
+       {skills.map((skill, index) => (
+          <li key={index} className="skill-item">
+            <img src={skill.logo} alt={`${skill.name} logo`} className="skill-logo" />
+            <span>{skill.name}</span>
+            </li>
+       ))}
       </ul>
     </section>
   )
