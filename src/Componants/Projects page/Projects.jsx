645ee73280eeabeb65258e6ed2d5b0910c1369c8
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-07-11 14:37:22 +0530

    projects changes

--- src/Componants/Projects page/Projects.jsx
+++ src/Componants/Projects page/Projects.jsx
@@ -1,19 +1,60 @@
 import React from 'react'
 import './projects.css'
+
+// local images
+import ecommerceImg from '../../assets/landing-page.jpg'
+import youtubeCloneImg from '../../assets/youtube_image.jpg'
+import ToDoListImg from '../../assets/to-do-list.jpg'
+import onlineCartImg from '../../assets/online shopping.jpg'
+
+const projectData = [
+  {
+    title: "To-Do-List app",
+    description: "A simple and efficient To-Do List application built using React JS. Users can add, delete, and mark tasks as completed. The app uses React state management for dynamic updates and offers a clean, minimal UI. Tasks persist during the session using local state ",
+    image: ToDoListImg,
+    github: "https://github.com/teja-reddy21/To-Do-app",
+
+  },
+  {
+    title: "E-commerce Store",
+    description: "e-commerce store built using React JS and  shopping site with cart functionality. Users can browse products, add items to their cart",
+    image: onlineCartImg,
+    github: "https://github.com/teja-reddy21/shopping-cart",
+  
+  },
+  {
+    title: "Youtube Clone",
+    description: "A responsive YouTube-inspired video streaming application built using React JS and integrated with the YouTube Data API",
+    image: youtubeCloneImg,
+    github:"https://github.com/teja-reddy21/youtube-clone",
+
+  },
+  {
+     title: "E-commerce page",
+    description: "A responsive and modern e-commerce landing page designed to showcase featured products, promotional offers, and brand identity. Built using React JS and custom CSS.",
+    image: ecommerceImg,
+    github: "https://github.com/teja-reddy21/page-loading",
+   
+  }
+];
+
 const Projects = () => {
+
   return (
      <section id="projects" className="projects">
       <h2>My Projects</h2>
       <div className="project-list">
-        <div className="project-card">
-          <h3>Portfolio Website</h3>
-          <p>Built using React JS and custom CSS.</p>
-        </div>
-        <div className="project-card">
-          <h3>E-commerce Store</h3>
-          <p>React + Firebase-based shopping site with cart functionality.</p>
-        </div>
-        {/* Add more cards */}
+        {projectData.map((project, index) => (
+          <div className="project-card" key={index}>
+            <img src={project.image} alt={project.title} className="project-image" />
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+            <div className="project-links">
+              <a href={project.github} target="_blank" rel="noopener noreferrer">GitHub</a>
+             
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   )
