452f7eb8ab8148bd2fda5c7b94f23ae89d13cd7a
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-07-12 17:15:47 +0530

    render the all sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 
-
-import Navbar from './Componants/Navbar/Navbar'
+import About from "./Componants/About page/About";
+import Contact from "./Componants/Contact page/Contact";
+import Home from "./Componants/Home page/Home";
+import Navbar from "./Componants/Navbar/Navbar";
+import Projects from "./Componants/Projects page/Projects";
+import Skills from "./Componants/Skills page/Skills";
+import './index.css'
 
 function App() {
   
@@ -8,8 +13,13 @@ function App() {
   return (
     <>
       <div>
-
-        <Navbar/>
+            <Navbar/>
+      <Home/>
+      <About/>
+      <Projects/>
+      <Skills/>
+      <Contact/>
+      
       </div>
     </>
   )
