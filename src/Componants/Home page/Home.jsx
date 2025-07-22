85f1296f97467967770df6a278667bed5157f2a0
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-07-22 21:56:20 +0530

    amination

--- src/Componants/Home page/Home.jsx
+++ src/Componants/Home page/Home.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import './home.css'
 import profileImg from '../../assets/profile.jpg';
+import { motion } from "motion/react"
 const Home = () => {
   return (
     <section id="home" className="home">
         <div className='home-content'>
+          <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+                duration: 0.5,
+                scale: { type: "spring", visualDuration: 0.5, bounce: 0.5 },
+            }}
+            
+        >
           <div className='profile-pic-container'>
             <img src={profileImg} alt='Teja Profile' className='profile-pic' />
           </div>
+        </motion.div>
+        
+          
        <div className='text-content'>
       <h1>Hi, I'm Teja</h1>
       <p>Frontend Developer | React JS Enthusiast</p>
-      <a href='/web developer.pdf' download className='resume-btn'>Download Resume</a>
+     
        </div>
        </div>
     </section>
