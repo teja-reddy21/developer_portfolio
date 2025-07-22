d05739f0a51a2d09a114c8a6a29febe4581c44ad
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-07-22 21:54:45 +0530

    heading hide

--- src/Componants/Navbar/Navbar.jsx
+++ src/Componants/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   
   return (
       <nav className="navbar">
-      <h2 className="logo">My Portfolio</h2>
+      <h2 className='logo1 hide-on-mobile'>Teja</h2>
       <ul className="nav-links">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
@@ -19,3 +19,5 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+
